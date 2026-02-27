e6f2f899509ec33c653faaa7dcd19ea021134c50
RisticAndjela <andjelaristic371@gmail.com>
2026-02-27 15:49:26 +0100

    fix: implemented spec 9
    not yet tested, but in simulation and db is working as expected

--- simulation/components/ir.py
+++ simulation/components/ir.py
@@ -14,35 +14,39 @@ def run_ir(settings, threads, stop_event):
 
     remote_map = {
         "0x300ff6897": "brgb_red",
+        "0xff6897": "brgb_red",
         "0x300ff9867": "brgb_green",
+        "0xff9867": "brgb_green",
         "0x300ffb04f": "brgb_blue",
+        "0xffb04f": "brgb_blue",
         "0x300ff4ab5": "brgb_off",
+        "0xff4ab5": "brgb_off",
     }
 
     def process_ir(code):
-        print(f"[{device_name}] Očitan IR kod: {code}")
+        code_n = _norm(code)
+        print(f"[{device_name}] Očitan IR kod: {code_n}")
 
-        if code in remote_map:
-            cmd = remote_map[code]
+        if code_n in remote_map:
+            cmd = remote_map[code_n]
             print(f"[{device_name}] Prepoznata komanda -> {cmd}")
             get_cmd_queue().put(cmd)
         else:
-            print(f"[{device_name}] Nepoznat kod: {code}")
+            print(f"[{device_name}] Nepoznat kod: {code_n}")
 
         ev_type = str(settings.get("type") or "ir_receiver").lower()
-
         event = SensorEvent(
             pi_id=str(pi_id),
             device=str(device_name).upper(),
             kind="sensor",
             type=ev_type,
             sensor_type=ev_type,
-            value=str(code),
+            value=str(code_n),
             simulated=bool(simulated),
             timestamp=time.time(),
         )
         event_queue.put(event)
-
+        
     if simulated:
         from simulation.simulators.ir import run_ir_simulator
         t = threading.Thread(target=run_ir_simulator, args=(process_ir, stop_event, remote_map), daemon=True)
@@ -52,3 +56,10 @@ def run_ir(settings, threads, stop_event):
 
     t.start()
     threads.append(t)
+    
+def _norm(code) -> str:
+    s = str(code).strip().lower()
+    if not s.startswith("0x"):
+        if all(c in "0123456789abcdef" for c in s):
+            s = "0x" + s
+    return s
--- simulation/simulators/ir.py
+++ simulation/simulators/ir.py
@@ -6,10 +6,10 @@ def run_ir_simulator(callback, stop_event, remote_map):
     keys.append("0xUNKNOWN_CODE") 
     
     print("Started IR simulator...")
-    
-    while not stop_event.is_set():
-        if random.random() < 0.2: 
-            code = random.choice(keys)
-            callback(code) 
         
-        time.sleep(5)
+    while not stop_event.is_set():
+        if random.random() < 0.2:
+            code = random.choice(keys)
+            callback(code)
+        time.sleep(2)              
+        time.sleep(2)              
